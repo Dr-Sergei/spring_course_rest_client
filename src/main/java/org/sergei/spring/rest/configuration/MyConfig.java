cee728e84a502c698d6e5c0f0297fd715ee0c162
Wittchen <swittche@ipb-halle.de>
2024-05-23 16:31:18 +0200

    phase 0.5: set-up pom.xml and created package configuration

---
+++ src/main/java/org/sergei/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package org.sergei.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sergei.spring.rest")
+public class MyConfig {
+}
